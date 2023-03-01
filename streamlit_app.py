ee1d18db3289e3e18a1a835e04013b6714dcb8ee
moralescnoel <64564192+moralescnoel@users.noreply.github.com>
2023-03-01 15:37:11 -0800

    Update streamlit_app.py
    Update bowl emoji syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 streamlit.title('Breakfast Favorites')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('\N{fondue} Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{fondue} Omega 3 & Blueberry Oatmeal \N{fondue}')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
