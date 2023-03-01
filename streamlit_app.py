feb651c2067a87d73ad86b6ebd67737353de2c3b
moralescnoel <64564192+moralescnoel@users.noreply.github.com>
2023-03-01 15:40:38 -0800

    Update streamlit_app.py
    Added Emojis images.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import streamlit
 streamlit.title('Breakfast Favorites')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('\N{fondue} Omega 3 & Blueberry Oatmeal \N{fondue}')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+Streamlit.text('🥑🍞 Avocado Toast')
